3bdb8044775f81b052c8975ee9b4ae8f7e5b71df
HelloIsReal <60082945+HelloIsReal@users.noreply.github.com>
2026-06-24 00:07:46 +1200

    Just extra comments for me to know what to do next

--- Main.java
+++ Main.java
@@ -8,6 +8,14 @@
 
 public class Main
 {
+    // Next things to work on
+    // Add options for the user to choose from. Create task, delete task,
+    // View tasks, change task catagory, change task value.
+
+    // Another extra thing to add is when a task is moved to the done catagory,
+    // it should ask the user what they did to finish the task.
+
+    // An extra extra thing to possibly add is GUI.
     private static taskManager tasky = new taskManager();
     public static void main(String[] args)
     {   
